c550e453e44e8e226872a4134535089a3f8592b8
thanhlong1997 <thanhlonghanam1997@gmail.com>
2019-11-29 16:02:43 +0700

    ddd

--- run_classifier.py
+++ run_classifier.py
@@ -58,7 +58,7 @@ flags.DEFINE_string(
 )
 
 flags.DEFINE_string(
-    "output_dir",'gs://test_bucket_share_1/model_trained/model_8_11',
+    "output_dir",'gs://test_bucket_share_1/model_trained/quora',
     "The output directory where the model checkpoints will be written."
 )
 
