6aa0039f5c2331540386eb6f5f8adf0c9fdee364
thanhlong1997 <thanhlonghanam1997@gmail.com>
2019-12-11 11:56:30 +0700

    ddd

--- run_classifier.py
+++ run_classifier.py
@@ -912,13 +912,13 @@ def main(_):
     assert num_written_lines == num_actual_predict_examples
 
 
-if __name__ == "__main__":
-  flags.mark_flag_as_required("data_dir")
-  flags.mark_flag_as_required("task_name")
-  flags.mark_flag_as_required("vocab_file")
-  flags.mark_flag_as_required("bert_config_file")
-  flags.mark_flag_as_required("output_dir")
-  tf.app.run()
+# if __name__ == "__main__":
+#   flags.mark_flag_as_required("data_dir")
+#   flags.mark_flag_as_required("task_name")
+#   flags.mark_flag_as_required("vocab_file")
+#   flags.mark_flag_as_required("bert_config_file")
+#   flags.mark_flag_as_required("output_dir")
+#   tf.app.run()
 
 import re
 class Bert_classifi(object):
